c3f3b9cdffe353e26935371afbe30e9a01eb3d8e
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-29 00:18:54 +0300

    Connecting User entity to db

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,49 @@
-export class User {}
+import { Order } from 'src/orders/entities/order.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+  // ENUM('Admin', 'Manager', 'Warehouse', 'Sales', 'Supplier'
+ export enum Role{
+    Admin = 'Admin',
+    Manager = 'Manager',
+    Warehouse = 'Warehouse',
+    Sales = 'Sales',
+    Supplier = 'Supplier'
+  }
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  first_name: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  last_name: string;
+
+  @Column({ unique: true, type: 'varchar', length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role })
+  role: Role;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @OneToMany(() => Order, (order) => order.user, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  orders: Order[];
+
+}
