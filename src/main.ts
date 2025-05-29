8d56b6f922f18548afdfb0de0ca3e84583b1ba21
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-29 22:19:59 +0300

    add http exception filter and global exception handling

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+// import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // detects DTO's and validates them globally
   app.useGlobalPipes(new ValidationPipe());
+  
+  // // Global exception filter to handle all exceptions
+  // app.useGlobalFilters(new AllExceptionsFilter());
 
   const configService = app.get(ConfigService);
   const PORT = configService.getOrThrow<number>('PORT');
