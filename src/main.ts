9c38dbd60d3e659b69f5ed6605d5154e66a56a39
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-03 13:06:39 +0300

    swagger UI

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
@@ -11,18 +12,33 @@ async function bootstrap() {
 
   // // Global exception filter to handle all exceptions
   // app.useGlobalFilters(new AllExceptionsFilter());
+  
+  // Swagger setup
+  const config = new DocumentBuilder()
+  .setTitle('Inventory API')
+  .setDescription('API documentation for the Inventory Management System')
+  .setVersion('1.0')
+  .build();
 
-  const configService = app.get(ConfigService);
-  const PORT = configService.getOrThrow<number>('PORT');
-  await app.listen(PORT, () => {
-    console.log(`Application is running on port: ${PORT}`);
-  });
+  const document = SwaggerModule.createDocument(app, config);
 
-  // get html file to be displayed on root
+ SwaggerModule.setup('/api/docs', app, document,
+    {
+      jsonDocumentUrl: '/api/docs-json',
+    }
+  );
+
   app.enableCors({
     origin: '*', // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     allowedHeaders: 'Content-Type, Authorization', // Allowed headers
   });
+  
+    const configService = app.get(ConfigService);
+    const PORT = configService.getOrThrow<number>('PORT');
+    await app.listen(PORT, () => {
+      console.log(`Application is running on port: ${PORT}`);
+    });
+    
 }
 bootstrap();
