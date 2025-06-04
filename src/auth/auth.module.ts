017e99e758e0b21b7d7e4777978e606800d509b5
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-04 17:00:43 +0300

    Refactor AuthModule to register JwtModule globally and set default strategy for PassportModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,13 +5,19 @@ import { DatabaseModule } from 'src/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AccessStrategy } from './strategies/accsess.stategy';
+import { RefreshStrategy } from './strategies/refresh.stategy';
 
 @Module({
   imports: [DatabaseModule,
     TypeOrmModule.forFeature([User]),
-    PassportModule.register({ defaultStrategy: 'access' })
+    JwtModule.register({
+      global: true,
+    }),// Register JwtModule globally
+    PassportModule, // Import PassportModule to use guards
   ],
+  providers: [AuthService, AccessStrategy, RefreshStrategy],
   controllers: [AuthController],
-  providers: [AuthService],
 })
 export class AuthModule {}
