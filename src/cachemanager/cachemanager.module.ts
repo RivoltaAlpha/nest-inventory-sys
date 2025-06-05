dc011b601da0a6e891290b3f41e4fc4cd99fa584
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-05 10:37:50 +0300

    deleted

--- src/cachemanager/cachemanager.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CachemanagerService } from './cachemanager.service';
-import { CachemanagerController } from './cachemanager.controller';
-
-@Module({
-  controllers: [CachemanagerController],
-  providers: [CachemanagerService],
-})
-export class CachemanagerModule {}
