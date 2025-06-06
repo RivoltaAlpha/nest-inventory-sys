8fba2b4fa6ed78e9dc053ba8240b37b42a235c45
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-06 23:59:01 +0300

    Refactor create and findOne methods in OrdersService to enhance user and product relation handling

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -11,18 +11,36 @@ export class OrdersService {
     private ordersRepository: Repository<Order>,
   ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    const order = this.ordersRepository.create(createOrderDto);
-    return this.ordersRepository.save(order);
-  }
+async create(createOrderDto: CreateOrderDto) {
+  const order = this.ordersRepository.create({
+    ...createOrderDto,
+    user: { user_id: createOrderDto.user_id }, // Link user relation
+    products: createOrderDto.products.map(p => ({ product_id: p.product_id })), // Link product relations
+  });
+  return this.ordersRepository.save(order);
+}
 
   findAll() {
     return this.ordersRepository.find();
   }
 
-  findOne(id: number) {
-    return this.ordersRepository.findOne({ where: { order_id: id } });
-  }
+// query builder to select specific fields and relations
+async findOne(id: number) {
+  return this.ordersRepository.createQueryBuilder('order')
+    .leftJoinAndSelect('order.user', 'user')
+    .select([
+      'order',
+      'user.user_id',
+      'user.first_name',
+      'user.last_name',
+      'user.email',
+    ])
+    .leftJoinAndSelect('order.products', 'product')
+    .leftJoinAndSelect('order.shipping', 'shipping')
+    .leftJoinAndSelect('order.returnEntity', 'returnEntity')
+    .where('order.order_id = :id', { id })
+    .getOne();
+}
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
     return this.ordersRepository.update(id, updateOrderDto);
@@ -31,4 +49,26 @@ export class OrdersService {
   remove(id: number) {
     return this.ordersRepository.delete(id);
   }
+
+  async findByUser(userId: number) {
+  return this.ordersRepository.find({
+    where: { user: { user_id: userId } },
+    relations: ['user', 'products'],
+    select: {
+      order_id: true,
+      total_price: true,
+      status: true,
+      user: {
+        first_name: true,
+        last_name: true,
+        email: true,
+      },
+      products: {
+        product_id: true,
+        name: true,
+        price: true,
+      },
+    },
+  });
+}
 }
