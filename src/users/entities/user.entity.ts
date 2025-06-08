15521a99ae544263ed73d018a96028c1bfea33ad
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-08 15:28:23 +0300

    Refactor updatedAt column definition for consistency

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -36,11 +36,7 @@ export class User {
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
-  @Column({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
-    onUpdate: 'CURRENT_TIMESTAMP',
-  })
+  @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP'})
   updatedAt: Date;
 
   @OneToMany(() => Order, (order) => order.user, {
