91e6c9dcc5948e7b0739adbfbab97a3420cbefd8
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-10 12:33:20 +0300

    Refactor RolesGuard to streamline role checking and remove unused imports

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,8 +1,5 @@
-import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Observable } from 'rxjs';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -12,42 +9,46 @@ import { ROLES_KEY } from '../decorators/role.decorators';
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(
-    private configService: ConfigService,
     private reflector: Reflector, // This is the Reflector service used to access metadata
     @InjectRepository(User) 
     private userRepository: Repository<User>, 
   ) {}
 
-  canActivate(
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+  ): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
         context.getHandler(),
         context.getClass(),
     ]);
-    if (!roles) {
+    
+    if (!requiredRoles) {
       return true; // If no roles are defined, allow access
     }
 
-    const request = this.getRequest(context);
+    const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    // Check if user has any of the required roles
-    const hasRole = () => user.roles.some((role) => roles.includes(role));
-    if (user && hasRole()) {
-      return true;
+    if (!user) {
+      return false; // If no user is found, deny access
     }
 
-    return false;
+    // fetch user from the db 
+    const verifiedUser = await this.userRepository.findOne({
+        where: { user_id: user.user_id },
+        select: ['user_id', 'email', 'role'], // Select only the necessary fields
+        });
+
+    // If the user is not found in the database, deny access
+    if (!verifiedUser) {
+      return false;
+    }
+
+    // Check if the user's role matches any of the required roles
+    return requiredRoles.some((role) => verifiedUser.role === role);
   }
 
-  getRequest(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    return request;
-  }
-  // use access token to get user roles
-    // and check if user has any of the required roles
-    // If user does not have the required roles, return false
-    // If user has the required roles, return true
-    
+
+    // use access token to get user roles 
+
 }
