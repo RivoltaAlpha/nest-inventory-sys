ea90fc04bc68792f27c5edc7a72fe6ec7b271162
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-10 15:12:10 +0300

    swagger configuration and helmet

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,35 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { AllExceptionsFilter } from './http-exception.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // detects DTO's and validates them globally
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());  // detects DTO's and validates them globally
+  app.use(helmet());
+  // app.setGlobalPrefix('api'); // Set a global prefix for all routes
 
-  // // Global exception filter to handle all exceptions
-  // app.useGlobalFilters(new AllExceptionsFilter());
-
+  // app.useGlobalFilters(new AllExceptionsFilter());  // Global exception filter to handle all exceptions
   // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Inventory API')
     .setDescription('API documentation for the Inventory Management System')
     .setVersion('1.0')
+    .addTag('Inventory', 'Inventory management endpoints')
+    .addTag('Users', 'User management endpoints')
+    .addTag('Auth', 'Authentication endpoints')
+    .addTag('Products', 'Product management endpoints')
+    .addTag('Categories', 'Category management endpoints')
+    .addTag('Suppliers', 'Supplier management endpoints')
+    .addTag('Orders', 'Order management endpoints')
+    .addTag('Transactions', 'Transaction management endpoints')
+    .addTag('Warehouses', 'Warehouse management endpoints')
+    .addTag('Shipping', 'Shipping management endpoints')
+    .addTag('Returns', 'Return management endpoints')
+    .addTag('Pricings', 'Pricing management endpoints')
+    .addTag('Seeding', 'Database seeding endpoints')
+    .addServer('http://localhost:8000', 'Local development server')
+    .addServer('https://nest-inventory-dmatg7afhndnbdgu.southafricanorth-01.azurewebsites.net/', 'Production server')
     .addBearerAuth(
       {
         type: 'http',
@@ -35,12 +50,19 @@ async function bootstrap() {
 
   SwaggerModule.setup('/api/docs', app, document, {
     jsonDocumentUrl: '/api/docs-json',
+    swaggerOptions: {
+      persistAuthorization: true, // Persist authorization in Swagger UI
+      tagsSorter: 'alpha', // Sort tags alphabetically
+      docExpansion: 'none', // Collapse all sections by default
+      customSiteTitle: 'Inventory API', // Customize the site title
+    },
   });
 
   app.enableCors({
     origin: '*', // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+    credentials: true, // Allow credentials
   });
 
   const configService = app.get(ConfigService);
