29c5806ec07d41da194f93a50b758ddbaea4d625
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-06-11 11:18:02 +0300

    comment caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,16 @@ import { ShippingModule } from './shipping/shipping.module';
 import { PricingsModule } from './pricings/pricings.module';
 import { ReturnsModule } from './returns/returns.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { SeedModule } from './seed/seed.module';
 import { LoggerMiddleware } from './logger.middleware';
 import { LogsModule } from './logs/logs.module';
 import { AppController } from './app.controller';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import { AuthModule } from './auth/auth.module';
-import { CacheableMemory} from 'cacheable';
-import { createKeyv, Keyv } from '@keyv/redis';
-import { CacheMetricsController } from './cache/metrics.controller';
+// import { CacheableMemory} from 'cacheable';
+// import { createKeyv, Keyv } from '@keyv/redis';
+// import { CacheMetricsController } from './cache/metrics.controller';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './auth/guards/at.guards';
 import { RolesGuard } from './auth/guards/roles.guard';
@@ -41,42 +41,41 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     PricingsModule,
     ReturnsModule,
     DatabaseModule,
-    
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
     SeedModule,
     LogsModule,
-    AuthModule,
-    CacheModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      isGlobal: true, // Make cache globally available
-      useFactory: (configService: ConfigService) => {
-        return {
-          ttl: 60000, // Default TTL for cache entries
-          stores: [
-            createKeyv(configService.getOrThrow<string>('REDIS_URL')),
-
-            // Using CacheableMemory for in-memory caching
-            new Keyv({
-              store: new CacheableMemory({ ttl: 30000, lruSize: 5000 }),
-            }),
-          ],
-          logger: true, // Enable logging for cache operations
-        };
-      },
-    }),
     TypeOrmModule.forFeature([User]), // Import User entity
+    AuthModule,
+    // CacheModule.registerAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   isGlobal: true, // Make cache globally available
+    //   useFactory: (configService: ConfigService) => {
+    //     return {
+    //       ttl: 60000, // Default TTL for cache entries
+    //       stores: [
+    //         createKeyv(configService.getOrThrow<string>('REDIS_URL')),
+
+    //         // Using CacheableMemory for in-memory caching
+    //         new Keyv({
+    //           store: new CacheableMemory({ ttl: 30000, lruSize: 5000 }),
+    //         }),
+    //       ],
+    //       logger: true, // Enable logging for cache operations
+    //     };
+    //   },
+    // }),
   ],
-  controllers: [AppController, CacheMetricsController],
-  // controllers: [AppController],
+  // controllers: [AppController, CacheMetricsController],
+  controllers: [AppController],
   providers: [
-    {
-      provide: 'APP_INTERCEPTOR',
-      useClass: CacheInterceptor, // Global cache interceptor
-    },
+    // {
+    //   provide: 'APP_INTERCEPTOR',
+    //   useClass: CacheInterceptor, // Global cache interceptor
+    // },
     {
       provide: APP_GUARD,
       useClass: AtGuard, // Global access token guard
